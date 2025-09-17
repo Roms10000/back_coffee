1bcc4555ef51d74ee53612119c1dfe728e2516ec
Roms10000 <romain.richardon@outlook.fr>
2025-09-17 09:07:31 +0200

    aa

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -3,30 +3,48 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
 
-class SecurityController extends AbstractController
-{
-    #[Route(path: '/register', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+class RegisterController extends AbstractController {
+    #[Route('/api/register', name: 'api_register', methods: ['POST'])]
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $em
+    ): JsonResponse {
+        $data = json_decode($request->getContent(), true);
 
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+        // Verifier si les donnés ont bien été saisie
+        if (!isset($data['email'], $data['password'], $data['name'], $data['first_name'])) {
+            return new JsonResponse(['error' => 'Missing fields'], 400);
+        }
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
-    }
+        // Verifier email si déja existant
+        $existingUser = $em->getRepository(User::class)->findOneBy(['email' => $data['email']]);
+            if ($existingUser) {
+            return new JsonResponse(['error' => 'Email already in use'], 409);
+            }
 
-    #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
-    {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        $user = new User();
+        $user->setEmail($data['email']);
+        $user->setName($data['name']);
+        $user->setFirstName($data['first_name']);
+        $user->setPassword($passwordHasher->hashPassword($user, $data['password']));
+        $user->setRoles(['ROLE_USER']); // par défaut
+
+        $em->persist($user);
+        $em->flush();
+
+        return new JsonResponse([
+            'status' => 'User created',
+            'id' => $user->getId(),
+            'name' => $user->getName()
+        ], 201);
     }
 }
